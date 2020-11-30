de6d33990bff6ec6396a91285f10d1924b954988
adrianmarino <adrianmarino@gmail.com>
2020-11-30 10:49:24 -0300

    feat: improve impl

--- clase/3/ejercicio-3.py
+++ clase/3/ejercicio-3.py
@@ -7,26 +7,40 @@ ordena en O(|L|) Lineal.
 """
 
 
-def sort(list):
+def numbers_index(numbers):
     """
-    Return ordered unrepeated elements.
-    - Precondition: elements in (0, 9) range.
-    - Order: O(n) Linear.
+    Retorna una lista de 0..9 elementos cada uno un boolean
+    que es True cuando el indice existe en numbers.
+
+    - Orden: O(n).
     """
-    values_count = [False] * 9
-    for e in list:
-        values_count[e] = True
+    number_index = [False] * 9
+    i = 0
+    while i < len(numbers):  # Itera n.
+        number_index[numbers[i]] = True
+        i += 1
+    return number_index
+
+
+def sort(numbers):
+    """
+    Ordena numbers ascendentemente.
+
+    - Precondiciones: Solo numeros del 0 al 9.
+    - Orden: O(n).
+    """
+    index = numbers_index(numbers)
 
     i = 0
-    result = []
-    while i < len(values_count):
-        if values_count[i]:
-            result.append(i)
+    sorted_numbers = []
+    while i < len(index):  # Itera del 0 al 9.
+        if index[i]:
+            sorted_numbers.append(i)
         i += 1
 
-    return result
+    return sorted_numbers
 
 
-list_1 = list(range(1, 5))
-list_1.reverse()
-print(f'\nData: {list_1} - Result: {sort(list_1)}')
+numbers = list(range(1, 5))
+numbers.reverse()
+print(f'\nInput: {numbers} -> Sorted: {sort(numbers)}')
